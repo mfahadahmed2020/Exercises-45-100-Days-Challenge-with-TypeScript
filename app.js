fa0d2ea68109b41dd4a5053e574bfc973baa268a
mfahadahmed2020 <160360284+mfahadahmed2020@users.noreply.github.com>
2024-03-24 03:13:01 +0500

    Personal Message

---
+++ app.js
@@ -0,0 +1,3 @@
+"use strict";
+let personname = "ERIC";
+console.log(`hello ${personname} would you like to learn some Python today?`);
---
+++ app.ts
@@ -0,0 +1,2 @@
+let personname: string= "ERIC";
+console.log(`hello ${personname} would you like to learn some Python today?`);
