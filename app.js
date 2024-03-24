462b6ab242d93a41e1968f5ddb98e284ed3df254
mfahadahmed2020 <160360284+mfahadahmed2020@users.noreply.github.com>
2024-03-25 01:18:02 +0500

    Task_14_Guest List

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 "use strict";
-let personname = "ERIC";
-console.log(`hello ${personname} would you like to learn some Python today?`);
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.guest_list = void 0;
+let guest_list = ['Ali', 'Farhan', 'Raza', 'Ayesha'];
+exports.guest_list = guest_list;
+for (let i = 0; i < guest_list.length; i++) {
+    console.log('Respected Sir/Madam' + guest_list[i] + '\n', 'We Invited You on Dinner Tomorrow.\n\nThank You');
+}
--- app.ts
+++ app.ts
@@ -1,2 +1,5 @@
-let personname: string= "ERIC";
-console.log(`hello ${personname} would you like to learn some Python today?`);
+let guest_list: string[] =[ 'Ali', 'Farhan', 'Raza', 'Ayesha'];
+for(let i=0; i<guest_list.length; i++){
+  console.log('Respected Sir/Madam' + guest_list[i] + '\n','We Invited You on Dinner Tomorrow.\n\nThank You')
+}
+export{guest_list}
