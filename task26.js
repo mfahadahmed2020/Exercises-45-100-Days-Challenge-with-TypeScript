1342d02a2d03ed42459cec498517727d2f57455e
mfahadahmed2020 <160360284+mfahadahmed2020@users.noreply.github.com>
2024-03-25 02:01:28 +0500

    Task_26_Alien Colors #2
    Alien Colors #2

---
+++ task26.js
@@ -0,0 +1,12 @@
+"use strict";
+let alien_color = "green";
+if (alien_color == "green") {
+    console.log("The Player Just Earned 5 Points for Shooting The Alien.");
+}
+else {
+    console.log("The Player Just Earned 10 Points.");
+}
+alien_color = "red";
+if (alien_color == "green") {
+    console.log("The Player Just Earned 5 Points for Shooting The Alien.");
+}
---
+++ task26.ts
@@ -0,0 +1,13 @@
+let alien_color: string ="green";
+
+
+if(alien_color =="green"){
+    console.log("The Player Just Earned 5 Points for Shooting The Alien.")
+}
+else{
+    console.log("The Player Just Earned 10 Points.")
+ }
+ alien_color = "red";
+ if(alien_color == "green"){
+    console.log("The Player Just Earned 5 Points for Shooting The Alien.")
+} 
