b848da537313f47e9277bb2d44a2ced403c89e69
mfahadahmed2020 <160360284+mfahadahmed2020@users.noreply.github.com>
2024-03-25 02:05:13 +0500

    Task_28_Stages of Life
    Stages of Life

---
+++ task28.ts
@@ -0,0 +1,24 @@
+let age: number = (25);
+if(age<2){
+    console.log("The Person is a Baby.")
+}
+
+else if(age>=2 && age<4){
+console.log("Person is a Toddler.")
+}
+
+else if(age>=4 && age<13){
+    console.log("Person is a Kid.")
+    }
+
+    else if(age>=13 && age<20){
+        console.log("Person is a Teen Ager.")
+        }
+    
+        else if(age>=20 && age<65){
+            console.log("Person is a Adult.")
+            }
+        
+            else{
+                console.log("Person is an Elder")
+            }
