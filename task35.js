c91b2c8cc1039f9d7e719386348fb4d536e5d1e3
mfahadahmed2020 <160360284+mfahadahmed2020@users.noreply.github.com>
2024-03-25 02:14:27 +0500

    Task_35_Animals
    Animals

---
+++ task35.js
@@ -0,0 +1,16 @@
+"use strict";
+// List of animals
+const animals = ['dog', 'cat', 'rabbit'];
+// Printing each animal's name
+console.log("Animals:");
+for (let animal of animals) {
+    console.log(animal);
+}
+// Printing a statement about each animal
+console.log("\nStatements about each animal:");
+for (let animal of animals) {
+    console.log(`A ${animal} would make a great pet.`);
+}
+// Printing what these animals have in common
+console.log("\nWhat these animals have in common:");
+console.log("Any of these animals would make a great pet!");
