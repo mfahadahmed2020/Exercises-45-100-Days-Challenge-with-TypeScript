db245c00ee8b50f58a129dbc9b7dc2767a03f10e
mfahadahmed2020 <160360284+mfahadahmed2020@users.noreply.github.com>
2024-03-25 01:15:26 +0500

    Task_13_Your Own Array

--- app.ts
+++ app.ts
@@ -1,2 +1,5 @@
-let personname: string= "ERIC";
-console.log(`hello ${personname} would you like to learn some Python today?`);
+// Excersise 13
+let transportation : string[] =['Civic','Bike','Bus','Suzuki Alto'];
+for(let i=0;i<transportation.length; i++){
+    console.log('I Would like to own a ' + transportation[i]);
+}
