5a2eb85ebdf87eaa29a2821ae892e16c104e0f5b
mfahadahmed2020 <160360284+mfahadahmed2020@users.noreply.github.com>
2024-03-25 01:32:23 +0500

    Task_016_More Guests
    More Guests

---
+++ 016.js
@@ -0,0 +1,12 @@
+"use strict";
+let guest_list = ['Ali', 'Farhan', 'Raza', 'Ayesha'];
+let not_present = ['Farhan'];
+let new_guest = ['Babar Azam'];
+guest_list[1] = new_guest[0];
+for (let i = 0; i < guest_list.length; i++) {
+    console.log('Respected Sir/Madam' + guest_list[i] + '\n', 'We Invited You on Dinner Tomorrow.\nThank You\n');
+}
+guest_list.unshift('Shaheen', 'Sarfaraz', 'Rizwan');
+for (let i = 0; i < guest_list.length; i++) {
+    console.log('Respected Sir/Madam' + guest_list[i] + '\n', 'We Invited You on Dinner Tomorrow.We Found A Bigger Dinner Table So We Decide to Invite 3 More Guest.\nThank You\n');
+}
