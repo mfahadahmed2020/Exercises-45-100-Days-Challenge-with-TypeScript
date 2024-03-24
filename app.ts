870a1943903dc07ba9dc7f4de4cc555500887ca0
mfahadahmed2020 <160360284+mfahadahmed2020@users.noreply.github.com>
2024-03-25 01:07:24 +0500

    Task_12_Greetings Start

--- app.ts
+++ app.ts
@@ -1,4 +1,12 @@
-let members: string[] =['azam','saqlain','memood','saba','sana'];
+// Exercise 11
+// let members: string[] =['azam','saqlain','mehmood','saba','sana'];
+// for(let i=0; i<members.length; i++){
+//    console.log(members[i]);
+// }
+
+// Exercise 12
+let members: string[] =['azam','saqlain','mehmood','saba','sana'];
+let message: string='kal birth day hai kis ki:';
 for(let i=0; i<members.length; i++){
-    console.log(members[i]);
+     console.log(message + members[i]);
 }
