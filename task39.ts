ab2536a4595a807ac54cdf2650f68593835af5d6
mfahadahmed2020 <160360284+mfahadahmed2020@users.noreply.github.com>
2024-03-25 23:21:17 +0500

    Task_39_City Names
    39_City Names

---
+++ task39.ts
@@ -0,0 +1,8 @@
+function cityCountry(city: string, country: string) {
+    return `${city}, ${country}`;
+}
+
+// Calling the function with at least three city-country pairs
+console.log(cityCountry("Lahore", "Pakistan"));
+console.log(cityCountry("New York", "USA"));
+console.log(cityCountry("London", "UK"));
