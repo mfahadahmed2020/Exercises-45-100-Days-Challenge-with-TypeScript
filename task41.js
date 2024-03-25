c1a2253909e2429ac83f91603928484a66d91712
mfahadahmed2020 <160360284+mfahadahmed2020@users.noreply.github.com>
2024-03-25 23:24:15 +0500

    Task_41_Magicians
    41_Magicians

---
+++ task41.js
@@ -0,0 +1,9 @@
+"use strict";
+let magicians = ["Harry Houdini", "David Blaine", "Penn & Teller", "Dynamo"];
+function showMagicians() {
+    for (let magician of magicians) {
+        console.log(magician);
+    }
+}
+// Call the function to display magicians
+showMagicians();
---
+++ task41.ts
@@ -0,0 +1,10 @@
+let magicians = ["Harry Houdini", "David Blaine", "Penn & Teller", "Dynamo"];
+
+function showMagicians() {
+    for (let magician of magicians) {
+        console.log(magician);
+    }
+}
+
+// Call the function to display magicians
+showMagicians();
