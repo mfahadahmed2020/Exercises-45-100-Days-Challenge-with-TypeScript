6da04602e735fc905d0c0817de087884bde12532
M Fahad Ahmed <160360284+mfahadahmed2020@users.noreply.github.com>
2024-03-27 00:52:01 +0500

    Task_47_LapTop
    47_LapTop

---
+++ task47.ts
@@ -0,0 +1,9 @@
+let laptops = [
+    { make: "Dell", model: "XPS 15", year: 2020 },
+    { make: "Samsung", model: "E 6350", year: 2024 },
+    { make: "HP", model: "Spectre x360", year: 2016 },
+  ];
+  let [laptop1, laptop2, laptop3 ] = laptops;
+  console.log(laptop1);
+  console.log(laptop2);
+  console.log(laptop3);
