23ace09ecbd6672ce499606ae9286f9da2cb907c
M Fahad Ahmed <160360284+mfahadahmed2020@users.noreply.github.com>
2024-03-28 02:30:00 +0500

    Task_58_Aversge Score
    Aversge Score

---
+++ T58.js
@@ -0,0 +1,10 @@
+"use strict";
+// This program calculates the average of all scores given
+function averageScore(...scores) {
+    // Adds all scores together and divides by the number of scores
+    let total = scores.reduce((sum, score) => sum + score, 0);
+    return total / scores.length;
+}
+// Example: finding the average of four scores
+console.log(averageScore(80, 90, 100, 70)); // Shows the average score
+// We add up all the scores, then divide by how many there are.
