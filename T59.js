c353d5e6a4770f5f29595f2cf365b68a0bf2f051
M Fahad Ahmed <160360284+mfahadahmed2020@users.noreply.github.com>
2024-03-28 02:31:56 +0500

    Task_59_Make Adder
    Make Adder

---
+++ T59.js
@@ -0,0 +1,12 @@
+"use strict";
+// This program makes a function that adds a specific number
+function makeAdder(valueToAdd) {
+    // This is the magic box. It takes a number and adds your special number to it
+    return function (number) {
+        return number + valueToAdd;
+    };
+}
+// Making a magic box that adds 5
+let addFive = makeAdder(5);
+console.log(addFive(10)); // If we put 10 in the box, it gives us 15
+// We made a function (magic box) that adds 5 to any number.
