a918d559ece05d627cc56f9722d6db5d17bb414e
M Fahad Ahmed <160360284+mfahadahmed2020@users.noreply.github.com>
2024-03-28 02:18:37 +0500

    Task_51_Calculate Area
    Calculate Area

---
+++ task51.ts
@@ -0,0 +1,14 @@
+// Original function for calculating the area of a rectangle
+
+function calculateArea(width: number, height: number): number {
+    return width * height;
+  }
+  
+  // Refactored into an arrow function
+  
+  let calculateAreaArrow = (width: number, height: number): number =>
+    width * height;
+  
+  // Example usage of the arrow function
+  
+  console.log(calculateAreaArrow(6, 7)); // Logs the area of the rectangle
