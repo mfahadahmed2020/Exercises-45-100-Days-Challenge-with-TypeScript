864ce876954169d6be6e0f8d3b3b76a4d403bfd5
M Fahad Ahmed <160360284+mfahadahmed2020@users.noreply.github.com>
2024-03-29 03:51:56 +0500

    Task_62_Blue Print
    Task_62_Blue Print
    Q62 - Making a Student Template: Create a blueprint for student information, including their name, age, and the classes they're taking, and then fill in this blueprint with an example student.

---
+++ T62.js
@@ -0,0 +1,10 @@
+"use strict";
+// Filling in the blueprint with an example student
+let student = {
+    name: "Alice",
+    age: 22,
+    courses: ["Math", "Science", "History"],
+};
+// Showing the student's information
+console.log(student);
+// We're using the blueprint to make sure our student has a name, age, and list of courses.
