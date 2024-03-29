2ce062bb1ca807787b1a76b6ee01bb57689fea91
M Fahad Ahmed <160360284+mfahadahmed2020@users.noreply.github.com>
2024-03-30 00:19:33 +0500

    Task_79_Creating Accessing Object Properties
    Task_79_Creating Accessing Object Properties
    
    Q79 - Creating and Accessing Object Properties: Construct an object representing a car with properties for make, model, and year. Then, show how you can access the model property of the car.

---
+++ T79.js
@@ -0,0 +1,10 @@
+"use strict";
+// This sets up an object for a car with specific details
+let car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 1982,
+};
+// Accessing and showing the car's model
+console.log(car.year); // Outputs: Corolla
+// We use dot notation (car.model) to get the model of the car from our object.
