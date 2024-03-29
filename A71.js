e1a4a1f29be93f45fe32b0813022863484e91722
M Fahad Ahmed <160360284+mfahadahmed2020@users.noreply.github.com>
2024-03-29 23:00:41 +0500

    Task_71_A_Compare Const
    Task_71_A_Compare Const
    
    Compare let and const: Create two examples where let allows reassignment but const does not. Try to reassign a const-declared variable and catch the error.

---
+++ A71.js
@@ -0,0 +1,13 @@
+"use strict";
+// Example with let and const
+let age = 30;
+age = 35; // Works fine because let allows reassignment
+console.log("Age:", age);
+const myName = "John";
+try {
+    "Doe"; // This line will cause an error
+}
+catch (error) {
+    console.log("Error: Can't reassign a const-declared variable."); // This message is shown
+}
+console.log("Name:");
