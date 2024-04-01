4acc9a19fa4743abfb3dcf804e224d6e6c99025b
M Fahad Ahmed <160360284+mfahadahmed2020@users.noreply.github.com>
2024-04-02 03:28:02 +0500

    Task_91_Create An Array
    Task_91_Create An Array
    
    Q91 - Create an array of your three favorite fruits and add a new fruit to the end of the array.

---
+++ T91.js
@@ -0,0 +1,6 @@
+"use strict";
+// Defines an array with three favorite fruits
+let favoriteFruits = ["Apple", "Banana", "Cherry"];
+favoriteFruits.push("Mango"); // Adds "Mango" to the end of the array
+console.log(favoriteFruits); // Outputs: ['Apple', 'Banana', 'Cherry', 'Mango']
+// This line adds a new fruit to our list of favorites.
