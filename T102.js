b03197cddf7380ca451fd6198347186cdaf947be
M Fahad Ahmed <160360284+mfahadahmed2020@users.noreply.github.com>
2024-04-11 18:45:24 +0500

    Task_102_Calculates Absolute
    Q 102 - Calculate and log the absolute difference between the number -5 and 5.
    
    Task_102_Calculates Absolute

---
+++ T102.js
@@ -0,0 +1,5 @@
+"use strict";
+// Calculates the absolute difference between -5 and 5
+const difference = Math.abs(-5 - 5);
+console.log(difference); // Outputs: 10
+// This line shows the absolute value of the difference between -5 and 5, ignoring the sign.
