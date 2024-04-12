99f115e3f4e1b960a806877050145f1bf88cbea7
M Fahad Ahmed <160360284+mfahadahmed2020@users.noreply.github.com>
2024-04-12 20:57:50 +0500

    Task_119_Hello World-5-Times
    Q 119 - Create a while loop that logs "Hello, World!" 5 times.
    
    Task_119_Hello World-5-Times

---
+++ T119.js
@@ -0,0 +1,9 @@
+"use strict";
+// Initializes a counter
+let count = 0;
+// This while loop runs until count is 5
+while (count < 5) {
+    console.log("Hello, World!"); // Logs "Hello, World!"
+    count++; // Increments count by 1
+}
+// Repeats logging "Hello, World!" 5 times, once per loop iteration.
