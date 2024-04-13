addbb4ccb4eebd57b89b50ac645d00759a8361a7
M Fahad Ahmed <160360284+mfahadahmed2020@users.noreply.github.com>
2024-04-14 02:11:00 +0500

    Task_130_Expotr Import JavaScript File
    Q 130 - Explain how to export a function from one JavaScript file and import it into another file.
    
    Task_130_Math Function
    
    Task_130_Expotr Import JavaScript File

---
+++ MathFunction.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = void 0;
+// In file: mathFunctions.ts
+const add = (a, b) => a + b;
+exports.add = add;
+// This line exports an add function from mathFunctions.ts
---
+++ T130.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// In another file where you want to use the add function:
+const MathFunction_1 = require("./MathFunction");
+console.log((0, MathFunction_1.add)(2, 3)); // Outputs: 5
+// Demonstrates importing the add function from mathFunctions.ts and using it.
