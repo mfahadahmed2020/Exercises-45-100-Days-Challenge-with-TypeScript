13ae3c72738a28ecf9525677794e7e335d4507c6
M Fahad Ahmed <160360284+mfahadahmed2020@users.noreply.github.com>
2024-04-14 02:16:32 +0500

    Task_132_Discuss JavaScript Modules Named
    Q 132 - Discuss the difference between default and named exports in JavaScript modules.
    
    Task_132_Discuss JavaScript Modules Named
    
    Calculator.ts
    
    Utils.ts
    
    T132.ts

---
+++ T132.ts
@@ -0,0 +1,7 @@
+// Importing named exports:
+import Utils from "./Utils";
+
+// Importing a default export:
+import Calculator from "./Calculator";
+
+// This code illustrates the syntax and usage differences between default and named exports.
